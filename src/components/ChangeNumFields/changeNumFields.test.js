7e4fa1c55f192360842ba1c504d0ea5cea64e5f7
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-05-13 08:31:50 +0200

    added test suite for changeNumFields

---
+++ src/components/ChangeNumFields/changeNumFields.test.js
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import * as React from 'react'
+import {render, screen, within } from '@testing-library/react'
+import ChangeNumFields from "./changeNumFields";
+import Form from '../Form/form';
+
+test('Renders ChangeNumFields', () => {
+  render(<Form />)
+  render(<ChangeNumFields />)
+  const changeNumFields = screen.getByText('Add more fields')
+  expect(changeNumFields).toBeInTheDocument()
+
+});
