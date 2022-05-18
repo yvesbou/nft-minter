b6d903fe069012a305ba0eb09dc9f25f9e2eed28
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-05-18 17:25:05 +0200

    re-introduced file upload with formik

--- src/components/FormSection/form.js
+++ src/components/FormSection/form.js
@@ -1,99 +1,26 @@
 import React, { useState } from "react"
 import UploadAndDisplayImage from 'components/ImageUpload/imageUpload';
 import ChangeNumFields from "components/ChangeNumFields/changeNumFields";
-import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
+import formik, { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 
-class MetadataField {
-    constructor(label = '', type = 'Text', value = '') {
-        this.label = label;
-        this.type = type;
-        this.value = value;
-    }
-}
 
 const FormSection = () => {
-    // const [metadataFields, setMetadataFields] = useState([new MetadataField()])
-
-    // const incrementFields = (e) => {
-    //     e.preventDefault();
-    //     setMetadataFields([...metadataFields, new MetadataField()]);
-    //   };
-
-    // const decrementFields = (e) => {
-    //     e.preventDefault();
-    //     if (metadataFields.length === 0) return;
-    //     setMetadataFields(prevState => 
-    //         [...prevState.slice(0,-1)]
-    //     )
-    // };
-
-    // const updateLabelInMetaDataField = (index, event) => {
-    //     const newLabel = event.target.value;
-    //     const metadataField = new MetadataField(newLabel, metadataFields[index].type, metadataFields[index].value);
-    //     setMetadataFields(prevState => 
-    //         [...prevState.slice(0,index), metadataField, ...prevState.slice(index+1)]
-    //     )
-    // }
-
-    // const updateTypeInMetaDataField = (index, event) => {
-    //     const newType = event.target.value;
-    //     const metadataField = new MetadataField(metadataFields[index].label, newType, metadataFields[index].value);
-    //     setMetadataFields(prevState => 
-    //         [...prevState.slice(0,index), metadataField, ...prevState.slice(index+1)]
-    //     )
-    // }
-
-    // const updateValueInMetaDataField = (index, event) => {
-    //     const newValue = event.target.value;
-    //     const metadataField = new MetadataField(metadataFields[index].label, metadataFields[index].type, newValue);
-    //     setMetadataFields(prevState => 
-    //         [...prevState.slice(0,index), metadataField, ...prevState.slice(index+1)]
-    //     )
-    // }
-
-    // const renderAdditionalInputFields = () => {
-    //     let inputFields = [];
-    //     //console.log(metadataFields);
-    //     metadataFields.forEach((field, i) => {
-    //         //console.log(field);
-    //         inputFields.push(
-    //             <li style={{"listStyle": "none"}} key={`${i}-additional-element`} >
-    //                 <input defaultValue={field.label} onChange={(e) => {
-    //                     updateLabelInMetaDataField(i, e);
-    //                 }} type="text" name={`${i}-additional-element`} id={`${i}-additional-element`} placeholder="key"></input>
-
-    //                 <select defaultValue={field.type} name="metadataFieldType" id="metadataFieldType" onChange={(e) =>{
-    //                     updateTypeInMetaDataField(i, e);
-    //                 }}>
-    //                     <option value="String">Text</option>
-    //                     <option value="List">List</option>
-    //                 </select>
-
-    //                 {field.type === 'String' && (
-    //                     <input  defaultValue={field.value} 
-    //                             onChange={(e) => {
-    //                                 updateValueInMetaDataField(i, e);
-    //                             }} 
-    //                             type="text" 
-    //                             name={`${i}-additional-element`} 
-    //                             id={`${i}-additional-element`} 
-    //                             placeholder="value" />
-    //                 )}
-    //                 {/* {field.type === 'List' && (
-    //                     renderAdditionalInputFields()
-    //                 )} */}
-    //             </li>
-    //         )
-    //     });
-    //     return inputFields
-    // }
+    const [file, setFile] = useState('')
     
+    const handleFileUpload = (event) => {
+        const reader = new FileReader();
+        const file = event.target.files[0];
+        reader.onloadend = () => {
+            setFile(reader.result)
+        };
+        reader.readAsDataURL(file);
+      }
+
     return (
         <Formik
-            initialValues={{ nameOfNFT: '', description: '' , additionalMetaDataFields: [{key: '', value: '', type: '1-Level'}]}}
+            initialValues={{ nameOfNFT: '', description: '' , image_name: '', image_type: ''}}
             validate={values => {
                 const errors = {};
-                
                 return errors;
             }}
             onSubmit={(values, { setSubmitting }) => {
@@ -103,7 +30,7 @@ const FormSection = () => {
                 }, 400);
             }}
         >
-        {({ values, isSubmitting }) => (
+        {({ values, isSubmitting, setFieldValue }) => (
             <Form>
                 <label htmlFor="nameOfNFT">Name of NFT</label>
                 <Field type="text" name="nameOfNFT" />
@@ -113,7 +40,20 @@ const FormSection = () => {
                 <Field type="text" as="textarea" name="description" />
                 <ErrorMessage name="description" component="div" />
                 <br></br>
-
+                <input
+                id="picture"
+                name="picture"
+                type="file"
+                onChange={(event) => {
+                    const file = event.target.files[0];
+                    setFieldValue("image_name", file.name);
+                    setFieldValue("image_type", file.type);
+                    handleFileUpload(event);
+                }}
+                />
+                <br/>
+                {file && <img width="300" height="300" src={file} /> }
+                <br/>
                 <button type="submit" disabled={isSubmitting}>
                     Submit
                 </button>
